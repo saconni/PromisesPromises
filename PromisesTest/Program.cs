80dcc40b33dbd93ebc33259ae8a485d6968c2053
saconni <saconni@gmail.com>
2018-06-01 01:30:06 -0300

    initial commit

---
+++ PromisesTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PromisesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Promise.Create((resolve, reject) =>
+            {
+                Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] starting");
+                resolve();
+            })
+            .Then((resolve, reject) =>
+            {
+                Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] done1");
+                    resolve();
+                });
+            })
+            .Then(() =>
+            {
+                Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] done2");
+            })
+            .Then((resolve, reject) =>
+            {
+                Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] done3");
+                    resolve();
+                });
+            })
+            .Then(() =>
+            {
+                Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] done4");
+            });
+
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] what are you waiting for?");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PromisesTest/Promise.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromisesTest
+{
+    class Promise
+    {
+        Action<Action, Action<Exception>> _action = null;
+
+        object _lock = new object();
+
+        Promise _nextPromise = null;
+
+        Action<Exception> _errorHandler = null;
+
+        bool _resolved = false;
+        bool _rejected = false;
+
+        Exception _ex = null;
+
+        public static Promise Create(Action<Action, Action<Exception>> action)
+        {
+            return new Promise(action);
+        }
+
+        public Promise(Action<Action, Action<Exception>> action) : this(true, action) { }
+
+        private Promise(bool immediate, Action<Action, Action<Exception>> action)
+        {
+            _action = action;
+            if(immediate)
+            {
+                Perform();
+            }
+        }
+
+        private void Perform()
+        {
+            try
+            {
+                _action(() => Resolve(), (ex) => OnReject(ex));
+            }
+            catch (Exception ex)
+            {
+                OnReject(ex);
+            }
+        }
+
+        private void OnReject(Exception ex)
+        {
+            lock (_lock)
+            {
+                _rejected = true;
+                _errorHandler?.Invoke(ex);
+            }
+        }
+
+        private void Resolve()
+        {
+            lock (_lock)
+            {
+                _resolved = true;
+                _nextPromise?.Perform();
+            }
+        }
+
+        public Promise Then(Action<Action, Action<Exception>> action)
+        {
+            _nextPromise = new Promise(false, action);
+
+            bool alreadyResolved = false;
+
+            lock (_lock)
+            {
+                if (_resolved)
+                {
+                    alreadyResolved = true;
+                }
+            }
+
+            if(alreadyResolved)
+            {
+                _nextPromise.Perform();
+            }
+
+            return _nextPromise;
+        }
+
+        public Promise Then(Action simpleAction)
+        {
+            return Then((resolve, reject) =>
+            {
+                try
+                {
+                    simpleAction.Invoke();
+                    resolve();
+                }
+                catch (Exception ex)
+                {
+                    reject(ex);
+                }
+            });
+        }
+
+        public void Catch(Action<Exception> action)
+        {
+            bool alreadyRejected = false;
+
+            lock (_lock)
+            {
+                if (_resolved)
+                {
+                    alreadyRejected = true;
+                }
+                else
+                {
+                    _errorHandler = action;
+                }
+            }
+
+            if (alreadyRejected)
+            {
+                action.Invoke(_ex);
+            }
+        }
+    }
+}
